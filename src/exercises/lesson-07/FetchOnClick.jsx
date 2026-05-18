3ee89f937edd10b82790e176550d71c897ba924e
jonastam93 <jonathan.stambuk@gmail.com>
2026-05-17 20:17:02 -0400

    commit

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,14 +1,24 @@
 import './Lesson07Styles.css';
+import { useState } from 'react';
+import { getSinglePost } from './api';
 
 export default function FetchOnClick() {
+  const [post, setPost] = useState(null);
+
+  async function handleGetPost() {
+    const data = await getSinglePost(1);
+    setPost(data);
+  }
   return (
-    <div className="root">
-      <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
-      <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
-      </div>
+    <div>
+      <button onClick={handleGetPost}>Get Post</button>
+
+      {post && (
+        <div>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      )}
     </div>
   );
 }
